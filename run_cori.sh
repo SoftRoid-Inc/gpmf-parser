10d0eb839c7c747b4f53e7674dbb9d544a328d24
takerfume <yoshida.taketo1995@gmail.com>
2022-06-20 07:27:17 +0000

    add demo_csv_cori

---
+++ run_cori.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Dockerfile entrypoint:
+# - If /input is a directory, run gpmfdemo on all contained files
+# - Otherwise, if any files matching /input.* exist, run gpmfdemo on them
+# - Otherwise, pass through arguments to gpmfdemo (e.g. samples/hero8.mp4 for that sample from this repository, which is copied into the container)
+
+# get directory of this run script
+cwd=`dirname "${0}"`
+
+if [ -d /input ]; then
+    find /input -maxdepth 1 -type f -print0 | \
+    xargs -0 -I{} $cwd/demo/gpmfdemo_cori "$@" {}
+else
+    find / -maxdepth 1 -type f -name 'input.*' -print0 | \
+    tee processed | \
+    xargs -0 -I{} $cwd/demo/gpmfdemo_cori "$@" {}
+
+    if ! [ -s processed ]; then
+        # didn't find any files beginning with /input.*
+        $cwd/demo/gpmfdemo_cori "$@"
+    fi
+fi
